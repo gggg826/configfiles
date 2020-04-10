8062b58c0eec75cfba5242e955bc19a91a05cdf0
gggg826 <liushida87@126.com>
2020-04-10 21:28:08 +0800

    Optimize SVNCompare

--- SVNCompare.bat
+++ SVNCompare.bat
@@ -4,3 +4,10 @@ set toolpath=%~dp0
 echo %~1> "%toolpath%temp.txt"
 echo %~2>> "%toolpath%temp.txt"
 "%toolpath%SPREADSHEETCOMPARE.EXE" "%toolpath%temp.txt"
+
+:: 如果C盘需要管理员权限，用下面命令
+:: set toolpath=C:\Program Files (x86)\Microsoft Office\Office16\DCF\
+:: set temppath=%~dp0
+:: echo %~1> "%temppath%temp.txt"
+:: echo %~2>> "%temppath%temp.txt"
+:: "%toolpath%SPREADSHEETCOMPARE.EXE" "%temppath%temp.txt"
